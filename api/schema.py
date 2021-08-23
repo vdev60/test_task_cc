42a2cde276ae10eaa889fbb209ff1eae88eb82e0
Dved <dved490@gmail.com>
2021-08-23 12:08:37 +0300

    Format code according to flake8

--- api/schema.py
+++ api/schema.py
@@ -1,19 +1,21 @@
 from graphene import ObjectType, String, Schema, Field, List
 import json
 
+
 class User(ObjectType):
     name = String()
     repos = List(String)
-    
+
+
 class Query(ObjectType):
     user = Field(User)
     repos = String()
 
     def resolve_user(root, info):
         with open("api/data.json", "r") as f:
-            data = json.load(f) 
-            
-        return User(name = data["user"], repos = data["repos"])
+            data = json.load(f)
 
-  
+        return User(name=data["user"], repos=data["repos"])
+
+
 schema = Schema(query=Query)
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask, request
 from flask import render_template
 from flask_graphql import GraphQLView
 from api.schema import schema
@@ -7,31 +7,34 @@ import json
 
 app = Flask(__name__)
 
-@app.route("/",methods=['GET','POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def get_user_data():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        response = requests.get(f"https://api.github.com/users/{username}")
+    if request.method == "POST":
+        username = request.form.get("username")
+        response = requests.get(
+            f"https://api.github.com/users/{username}")
         if response:
-            data = requests.get(f"https://api.github.com/users/{username}/repos")
+            data = requests.get(
+                f"https://api.github.com/users/{username}/repos")
             repos = [i["html_url"] for i in data.json()]
             with open("api/data.json", "w") as f:
-                data_set = {"user":username, "repos": repos}
-                json.dump(data_set, fp=f, indent = 4)
-            return render_template("list_repos.html", repos=repos , username = username)
+                data_set = {"user": username, "repos": repos}
+                json.dump(data_set, fp=f, indent=4)
+            return render_template(
+                "list_repos.html",
+                repos=repos,
+                username=username)
         else:
-            return f"<h1>User not found</h1>"
-            
+            return "<h1>User not found</h1>"
+
     return render_template("form.html")
 
-app.add_url_rule(
-       '/graphql',
-       view_func=GraphQLView.as_view(
-           'graphql',
-           schema=schema,
-           graphiql=True
-       )
-   )
 
-if __name__ == '__main__':
+app.add_url_rule(
+    "/graphql",
+    view_func=GraphQLView.as_view("graphql", schema=schema, graphiql=True)
+)
+
+if __name__ == "__main__":
     app.run()
