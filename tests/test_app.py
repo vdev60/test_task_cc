881f2c1a26114474098c5e8f77ee352804d420fd
Dved <dved490@gmail.com>
2021-08-26 20:16:45 +0300

    Update unit testing

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,17 +1,17 @@
-from app import app 
+from app import app
 import pytest
-from unittest.mock import MagicMock
 
 
 @pytest.fixture
 def client():
     return app.test_client()
 
+
 def test_home(client):
-    response = client.get('/')
+    response = client.get("/")
     assert response.status_code == 200
+
 
 def test_post_request(client):
     response = client.post("/")
     assert response.status_code == 200
-
--- tests/test_graphql.py
+++ tests/test_graphql.py
@@ -2,13 +2,16 @@ from graphene.test import Client
 from api import schema
 import pytest
 
+
 @pytest.fixture
 def client():
     client = Client(schema.schema)
     return client
 
+
 def test_user_name_repos(snapshot, client):
-    snapshot.assert_match(client.execute('''{ user{name repos} }''')) 
+    snapshot.assert_match(client.execute("""{ user{name repos} }"""))
+
 
 def test_user_name(snapshot, client):
-    snapshot.assert_match(client.execute('''{ user{name} }''')) 
+    snapshot.assert_match(client.execute("""{ user{name} }"""))
